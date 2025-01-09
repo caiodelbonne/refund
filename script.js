e6882be70ba1a9db7daeda3354edf2588d16043a
Caio Cesar <caiocesar_01@hotmail.com>
2025-01-09 18:36:22 -0300

    funcao BRL

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ const amount = document.getElementById("amount")
 
 amount.oninput = () => { // capt entrada e tirando as letras do input com regex
     let value = amount.value.replace(/\D/g,"") // aceita o valor atual sem letras
-    
-    amount.value = value
+    // transformar em centavos
+
+    value = Number(value) / 100
+
+    amount.value = formatCurrencyBRL(value)
 
 } 
+
+function formatCurrencyBRL(value) {
+    // formata no padrao brl
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency:"BRL",
+    })
+
+    return value
+}
