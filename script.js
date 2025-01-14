7b30f0806138f07f7a47daa9321a5ab5ddf79511
Caio Cesar <caiocesar_01@hotmail.com>
2025-01-14 14:10:53 -0300

    function  entry form

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // selecionar elementos  
-const amount = document.getElementById("amount") 
+const form = document.querySelector('form')
 
+const amount = document.getElementById("amount") 
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 amount.oninput = () => { // capt entrada e tirando as letras do input com regex
     let value = amount.value.replace(/\D/g,"") // aceita o valor atual sem letras
     // transformar em centavos
-
     value = Number(value) / 100
 
+    // variavel recebe o valor chama funçao recebendo o valor transformando em real 
     amount.value = formatCurrencyBRL(value)
 
 } 
@@ -20,4 +23,19 @@ function formatCurrencyBRL(value) {
     })
 
     return value
+}
+
+form.onsubmit= (event) =>{
+    event.preventDefault()
+//  nova dispesa
+    const newExpense = { 
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name:category.options[category.selectedIndex].text , //seleciona a oopcao selecionada 
+        amount: amount.value,
+        created_at: new Date() ,
+    }
+
+    console.log(newExpense)
 }
